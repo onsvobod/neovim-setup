e6dc020ba5e3e5858f2850cf96eba98773431ce5
Ondřej Svoboda <osvoboda@protonmail.com>
2019-04-07 10:59:34 +0200

    some fixes

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+apt-get update
+
+apt-get install python-dev python-pip python3-dev python3-pip
+
 # debian neovim and python plugins install
-apt-get install neovim python-neovim python3-neovim
+apt-get install neovim
+echo "deb http://deb.debian.org/debian unstable main" >> /etc/apt/sources.list
+apt-get update
 
 # ubuntu neovim and python plugins install
 #apt-get install software-properties-common
 #add-apt-repository ppa:neovim-ppa/stable
 #apt-get update
 #apt-get install neovim
-#apt-get install python-dev python-pip python3-dev python3-pip
-#pip3 install --user --upgrade pynvim
-#pip2 install --user --upgrade pynvim
+
+pip2 install --user --upgrade pynvim
+pip3 install --user --upgrade pynvim
 
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim
@@ -24,3 +30,6 @@ pip install python-language-server
 pip3 install python-language-server
 pip install 'python-language-server[all]'
 pip3 install 'python-language-server[all]'
+
+#plugin manager install
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
