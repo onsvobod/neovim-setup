431eea8cffb4317b8214553cee9916a7961961f9
Ondřej Svoboda <osvoboda@protonmail.com>
2019-04-10 11:31:12 +0200

    using default config

--- install.sh
+++ install.sh
@@ -27,6 +27,8 @@ sudo apt-get install  -t stretch-backports clang-tools-6.0
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-6.0 100
 
 #python language server
+pip install jedi
+pip3 install jedi
 pip install python-language-server
 pip3 install python-language-server
 pip install 'python-language-server[all]'
@@ -36,5 +38,3 @@ pip3 install 'python-language-server[all]'
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sudo apt-get install ack-grep
-
-cp pylintrc ~/.config
