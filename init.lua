a0f2dc578a2f7023e3dfcdb8f953987b0a756e16
Ondrej Svoboda <onsvobod@gmail.com>
2024-02-14 22:31:53 +0100

    port keybinds to lua

--- init.lua
+++ init.lua
@@ -106,3 +106,27 @@ useTabs('*.go')
 setTabWidth2('*.yaml')
 setTabWidth2('*.yml')
 setTabWidth2('*.json')
+
+-------------------------------------------------------------------------------
+--------------------------------- Keybinds ------------------------------------
+-------------------------------------------------------------------------------
+
+local map = vim.keymap.set
+
+g.mapleader = ','                                                           -- Set <leader>
+map('i', '<c-c>', '<ESC>')                                                  -- Ctrl-c -> Esc
+map('', '<c-t>h', ':tabp<Enter>')                                           -- Ctrl-t + h -> move tab left
+map('', '<c-t>l', ':tabn<Enter>')                                           -- Ctrl-t + l -> move tab right
+map('t', '<Esc>', "<C-\\><C-n>")                                            -- Esc -> escape :terminal
+map('n', '<C-k>', ':call LanguageClient#textDocument_definition()<Enter>')  -- Ctrl-k -> GoTo definition
+map('n', '<C-l>', ':call LanguageClient#textDocument_references()<Enter>')  -- Ctrl-l -> GoTo reference
+map('n', '<F7>', ':AsyncRun -program=make<Enter>')                          -- F7 -> Run makeprg
+--map('', '<C-n>', ':NERDTreeToggle<CR>')                                   -- Ctrl-n -> Open NerdTree
+map('n', '<c-G>', ':Grepper -tool grep -cword -noprompt<Enter>')            -- Ctrl-g -> Run Grepper on word under cursor
+map('n', '<F8>', ':GdbStart gdb -q<Enter>')                                 -- F8 -> Start gdb
+-- Enter -> Select completion
+map('i', '<Enter>', 'pumvisible() ? "\\<c-y>\\<cr>" : "\\<CR>"', {expr = true})
+-- Tab -> Next completion
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+-- " Shift-Tab -> Previous completion
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
